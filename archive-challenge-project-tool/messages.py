4808533758c4fee945533b940ba3e9c56f64ffa8
Thomas Yu <thomasyu888@gmail.com>
2018-06-07 14:05:56 -0700

    Configure for MM challenge writeups

--- archive-challenge-project-tool/messages.py
+++ archive-challenge-project-tool/messages.py
@@ -16,9 +16,9 @@ dry_run = False
 
 ## Edit these URLs to point to your challenge and its support forum
 defaults = dict(
-    challenge_instructions_url = "https://www.synapse.org/...",
-    support_forum_url = "https://www.synapse.org/#!Synapse:{synIdHere}/discussion/default",
-    scoring_script = "the archiver")
+    challenge_instructions_url = "https://www.synapse.org/#!Synapse:syn6187098/wiki/463671",
+    support_forum_url = "https://www.synapse.org/#!Synapse:syn6187098/discussion/default",
+    scoring_script = "The MM Challenge writeup-archiver")
 
 ##---------------------------------------------------------
 ## Message templates:
